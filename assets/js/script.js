6803bba3778a192cf979e9a3e5ca51b0741e67ae
Nicolas Kvaternik <nicolaskvaternik@gmail.com>
2019-05-21 14:32:00 +0200

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
-$('#wrapper').click(function () {
-  $('.icon').toggleClass('close');
+$(function (){
+  // menu burger
+  $('#wrapper').click(function () {
+    $('.icon').toggleClass('close');
+  });
+  // carousel
+  $('.owl-carousel').owlCarousel({
+    items:1,
+      loop:true,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:5000,
+      autoplayHoverPause:true
+  });
 });
